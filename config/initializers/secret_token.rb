3593b9e9cf9190df4e2b7cecd2caa4646b202092
divine <divine@divine-Satellite-C650>
2014-07-18 17:27:37 +0300

    sign in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalentShow::Application.config.secret_token = '59d94fbc09be42177ced30b39eb43b6441b5bbe71cfdd15713b3af76005be47dde24edb8fe16f3e8b2097c4d8740e0d17cee57e8ebd6f793fd9d3bed70ee3c54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TalentShow::Application.config.session_store :cookie_store, key: '_Talent_show_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TalentShow::Application.config.session_store :active_record_store
